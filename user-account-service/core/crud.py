14f968fbd3bf60852a2db50eac95bd0d1ad2dd50
Stonetech66 <maxstonne66@gmail.com>
2023-07-24 04:26:33 +0100

    edit user crud class
    Signed-off-by: Stonetech66 <maxstonne66@gmail.com>

--- user-account-service/core/crud.py
+++ user-account-service/core/crud.py
@@ -2,27 +2,21 @@ from .models import User
 from fastapi import HTTPException
 from sqlalchemy import select
 import uuid
-from passlib.context import CryptContext
+from .utils import password_manager
 
-pwd_hash=CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-
-def hash_password(password):
-    return pwd_hash.hash(password)
-def verify_password(plain_password, hashed_password):
-    return pwd_hash.verify(plain_password, hashed_password)
 
 class UserCrud:
 
     
-
+    @staticmethod
     async def get_user_by_id(database, id):
         query= select(User).where(User.c.id==id)
         user= await database.fetch_one(query)
         return user
 
+    @staticmethod
     async def create_user(database, schema):
-        schema.password=hash_password(schema.password)
+        schema.password=password_manager.hash_password(schema.password)
         query= User.insert().values(**schema.dict(), id=str(uuid.uuid4()))
         try:
             user= await database.execute(query)
@@ -30,17 +24,18 @@ class UserCrud:
         except:
             raise HTTPException(detail='user with this email already exists', status_code=400)
 
-    
+    @staticmethod
     async def get_user_by_email(database, email):
         query= select(User).where(User.c.email==email)
         user= await database.fetch_one(query)
         return user
 
+    @staticmethod
     async def authenticate(database, email, password):
         query= select(User).where(User.c.email==email)
         user= await database.fetch_one(query)
         if user:
-            if verify_password(password, user.password):
+            if password_manager.verify_password(password, user.password):
                 return user
             raise HTTPException(detail="invalid email or password", status_code=401)
         raise HTTPException(detail="invalid email or password", status_code=401)
---
+++ user-account-service/core/utils.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+pwd_hash=CryptContext(schemes=['bcrypt'], deprecated='auto')
+class PasswordManager:
+    pwd_hash=CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+    @classmethod
+    def hash_password(cls, password):
+        return cls.pwd_hash.hash(password)
+    @classmethod
+    def verify_password(cls, plain_password, hashed_password):
+        return cls.pwd_hash.verify(plain_password, hashed_password)
+    
+
+password_manager=PasswordManager
